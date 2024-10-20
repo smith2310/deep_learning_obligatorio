86303164bbaf23a9ea17647fb7977e465ffc1f6a
Martin <mym2310@gmail.com>
2024-10-19 21:26:34 -0300

    Agregue una notebook como main y clases de utils y un custom dataset para cargar los datos de kaggel

---
+++ SegmentationDataset.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+from PIL import Image
+import torchvision.datasets as datasets
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import v2 as T 
+from typing import Callable, Optional
+
+class SegmentacionDataset(Dataset):
+    """
+    Dataset que se encarga de cargar los datos y resultados para el dataset provisto en Kaggle
+    """
+
+    def __init__(
+            self,
+            root_dir: Path,
+            load_mask: bool,
+            image_format: str = 'RGB',
+            mask_format: str = 'L',
+            transform: Optional[Callable] = None
+    ):
+        super().__init__()
+        self.root_dir = root_dir
+        self.load_mask = load_mask
+        self.image_format = image_format
+        self.mask_format = mask_format
+        self.transform = transform
+        self.image_names = os.listdir(root_dir/'images')
+
+    def __len__(self):
+        return len(self.image_names)
+    
+    def __getitem__(self, index):
+        image_name = self.image_names[index]
+        image_path = self.root_dir / 'images' / image_name
+        image = Image.open(image_path).convert(self.image_format)
+        if self.load_mask:
+            mask_path = self.root_dir / 'masks' / image_name
+            mask = Image.open(mask_path).convert(self.mask_format)
+        else:
+            mask = None
+
+        if self.transform:
+            image = self.transform(image)
+            if mask:
+                mask = self.transform(mask)
+
+        return image, mask
